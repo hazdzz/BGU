e6b74c1b8cc74864fc1557c1d4de7500cde3d367
Chieh Chang <raphaelpeo@gmail.com>
2024-09-10 07:44:32 +0800

    replace sigmoid(x) with 0.5 * (1 + tanh(0.5 * x))

--- bgu.py
+++ bgu.py
@@ -8,7 +8,9 @@ class BinaryGatingUnit(nn.Module):
         super(BinaryGatingUnit, self).__init__()
 
     def forward(self, input: Tensor) -> Tensor:
-        p = torch.sigmoid(input)
+        # p = torch.sigmoid(input)
+        # sigmoid(x) = 0.5 * (1 + tanh(0.5 * x))
+        p = 0.5 * (1 + torch.tanh(0.5 * input))
         bern = torch.bernoulli(p)
         eps = torch.where(bern == 1, 1 - p, -p)
         gate = p + eps
