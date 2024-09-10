6f4255f02d836725c724b1069d8c073458fc2c28
Chieh Chang <raphaelpeo@gmail.com>
2024-09-10 10:25:59 +0800

    improve bgu with more implementations

--- bgu.py
+++ bgu.py
@@ -4,13 +4,20 @@ from torch import Tensor
 
 
 class BinaryGatingUnit(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, act_func: str = 'sigmoid') -> None:
         super(BinaryGatingUnit, self).__init__()
+        assert act_func in ['sigmoid', 'tanh', 'erf']
+        self.act_func = act_func
 
     def forward(self, input: Tensor) -> Tensor:
-        # p = torch.sigmoid(input)
-        # sigmoid(x) = 0.5 * (1 + tanh(0.5 * x))
-        p = 0.5 * (1 + torch.tanh(0.5 * input))
+        if self.act_func == 'sigmoid':
+            p = torch.sigmoid(input)
+        elif self.act_func == 'tanh':
+            p = 0.5 * (1.0 + torch.tanh(0.5 * input))
+        elif self.act_func == 'erf':
+            p = 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
+        else:
+            raise NotImplementedError(f'ERROR: The {self.act_func} based bgu is undefined.')
         bern = torch.bernoulli(p)
         eps = torch.where(bern == 1, 1 - p, -p)
         gate = p + eps
