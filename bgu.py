a9d74d48489ef86ae0089c800dfeaa68cfd86d49
hazdzz <raphaelpeo@gmail.com>
2025-02-12 15:04:38 +0800

    improve torch.bernoulli through F.gumbel_softmax

--- bgu.py
+++ bgu.py
@@ -1,17 +1,21 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch import Tensor
 
 
 class BinaryGatingUnit(nn.Module):
     def __init__(self) -> None:
         super(BinaryGatingUnit, self).__init__()
+        self.tau = nn.Parameter(torch.ones(1))
 
-    def forward(self, input: Tensor) -> Tensor:
-        p = torch.sigmoid(input)
-        bern = torch.bernoulli(p)
-        eps = torch.where(bern == 1, 1 - p, -p)
-        gate = p + eps
-        bgu = input * gate
+    def forward(self, x: Tensor) -> Tensor:
+        prob = torch.sigmoid(x)
+        logits = torch.stack([prob, 1 - prob], dim=-1)
+        gumbel_mask = F.gumbel_softmax(logits, tau=self.tau, hard=True)
+        bern = gumbel_mask[..., 0]
+        bias = torch.where(bern == 1, 1 - prob, -prob)
+        mask = prob + bias
+        bgu = x * mask
 
         return bgu
